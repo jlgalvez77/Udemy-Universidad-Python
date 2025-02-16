6316410ddcd821794c3758e11a1528e27e6f8f90
Jose Galvez <jlgalvez77@gmail.com>
2025-02-16 23:05:35 +0100

    16/02/25

--- Clases-Objetos/get_set.py
+++ Clases-Objetos/get_set.py
@@ -48,9 +48,11 @@ if __name__ == '__main__':
     coche1 = Coche('Toyota', 'Yaris', 'Azul')
     coche1.conducir()
 
-    coche1.set_marca('Toyota 2')
-    coche1.set_modelo('Yaris 2')
-    coche1.set_color('Azul 2')
-    coche1.conducir()
+    # coche1.set_marca('Toyota 2')
+    # coche1.set_modelo('Yaris 2')
+    # coche1.set_color('Azul 2')
+    # coche1.conducir()
     # Atributo de marca del coche1
+    print(f'Atributo marca coche1: {coche1.marca}')
+    coche1.marca = 'Toyota 2'
     print(f'Atributo marca coche1: {coche1.marca}')
